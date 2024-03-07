11017b2ea6b82df7370eb71f3437cb38c71f387c
betamemo <bandita.kul@gmail.com>
2024-03-07 11:36:31 +0700

    add method randomAndRound

--- m2-java-practice/src/main/java/com/harbourspace/lesson06/MathTask.java
+++ m2-java-practice/src/main/java/com/harbourspace/lesson06/MathTask.java
@@ -2,6 +2,10 @@ package com.harbourspace.lesson06;
 
 public class MathTask {
 
-    // solve your tasks here
+    public static double randomAndRound(double min, double max, int decimalPlaces) {
+        double randomNumber = Math.random() * (max - min) + min;
+        double roundNumber = Math.round(randomNumber * Math.pow(10, decimalPlaces)) / Math.pow(10, decimalPlaces);
+        return roundNumber;
+    }
 
 }
