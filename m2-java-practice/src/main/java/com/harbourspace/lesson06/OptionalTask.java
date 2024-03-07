4e15011a0c5abc43ec677e8df623406ce79b73e9
betamemo <bandita.kul@gmail.com>
2024-03-07 11:40:06 +0700

    add method Optional and find the word

--- m2-java-practice/src/main/java/com/harbourspace/lesson06/OptionalTask.java
+++ m2-java-practice/src/main/java/com/harbourspace/lesson06/OptionalTask.java
@@ -1,7 +1,29 @@
 package com.harbourspace.lesson06;
 
+import java.util.Optional;
+
 public class OptionalTask {
 
-    // solve your tasks here
+    public static Optional<String> getWord(String text, String word) {
+        if (text.contains(word)) {
+            return Optional.of(word);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static void main(String[] args) {
+        String text = "Chocolate is a food product made from roasted and ground cocoa pods mixed with fat and powdered sugar to produce a solid confectionery.";
+        System.out.println(text);
+
+        String word = "Cake";
+        Optional<String> result = getWord(text, word);
+
+        if (result.isPresent()) {
+            System.out.println("Word found: " + result.get());
+        } else {
+            System.out.println("Word not found: " + word);
+        }
+    }
 
 }
