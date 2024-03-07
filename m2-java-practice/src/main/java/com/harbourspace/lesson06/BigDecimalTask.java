75df8188fbb7e5e508c19fe3774fff42ac776fa8
betamemo <bandita.kul@gmail.com>
2024-03-07 09:50:07 +0700

    add method calculateArea

--- m2-java-practice/src/main/java/com/harbourspace/lesson06/BigDecimalTask.java
+++ m2-java-practice/src/main/java/com/harbourspace/lesson06/BigDecimalTask.java
@@ -1,7 +1,18 @@
 package com.harbourspace.lesson06;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BigDecimalTask {
 
-    // solve your tasks here
+    public BigDecimal calculateArea(int radius, int decimalPlaces) {
+        BigDecimal r = BigDecimal.valueOf(radius);
+        BigDecimal pi = BigDecimal.valueOf(Math.PI);
+
+        BigDecimal area = r.pow(2).multiply(pi);
+        BigDecimal round = area.setScale(decimalPlaces, RoundingMode.HALF_EVEN);
+
+        return round;
+    }
 
 }
