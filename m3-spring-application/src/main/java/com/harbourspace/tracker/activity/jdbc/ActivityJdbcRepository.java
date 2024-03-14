3cb113affc710424ef26b6fe7094459fcf5f347c
betamemo <bandita.kul@gmail.com>
2024-03-14 22:04:19 +0700

    reformat code

--- m3-spring-application/src/main/java/com/harbourspace/tracker/activity/jdbc/ActivityJdbcRepository.java
+++ m3-spring-application/src/main/java/com/harbourspace/tracker/activity/jdbc/ActivityJdbcRepository.java
@@ -5,6 +5,7 @@ import com.harbourspace.tracker.activity.model.NewActivity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,48 +23,48 @@ public class ActivityJdbcRepository {
 
     public List<Activity> selectAll() {
         logger.debug("Selecting all activity");
-        return jdbcTemplate.query("SELECT * FROM activity", (resultSet, index) -> new Activity(
-                resultSet.getLong("id"),
-                resultSet.getLong("user_id"),
-                resultSet.getString("type"),
-                resultSet.getString("name"),
-                resultSet.getDouble("kcal_per_minute")
-        ));
+        return jdbcTemplate.query("SELECT * FROM activity",
+                (resultSet, index) -> new Activity(
+                        resultSet.getLong("id"),
+                        resultSet.getLong("user_id"),
+                        resultSet.getString("type"),
+                        resultSet.getString("name"),
+                        resultSet.getDouble("kcal_per_minute")
+                ));
     }
 
     public Activity selectById(Long id) {
         logger.debug("Selecting activity " + id);
-        return jdbcTemplate.query("SELECT * FROM activity WHERE id = ? LIMIT 1", this::rowMapper, id)
-                .stream()
-                .findFirst()
-                .orElse(null);
+        return jdbcTemplate.query("SELECT * FROM activity WHERE id = ? LIMIT 1",
+                this::rowMapper, id
+        ).stream().findFirst().orElse(null);
     }
 
     public List<Activity> selectByUserId(Long userId) {
         logger.debug("Selecting activity " + userId);
-        return jdbcTemplate.query("SELECT * FROM activity", (resultSet, index) -> new Activity(
-                resultSet.getLong("id"),
-                resultSet.getLong("user_id"),
-                resultSet.getString("type"),
-                resultSet.getString("name"),
-                resultSet.getDouble("kcal_per_minute")
-        ));
+        return jdbcTemplate.query("SELECT * FROM activity WHERE user_id = ?",
+                new Object[]{userId}, (resultSet, index) -> new Activity(
+                        resultSet.getLong("id"),
+                        resultSet.getLong("user_id"),
+                        resultSet.getString("type"),
+                        resultSet.getString("name"),
+                        resultSet.getDouble("kcal_per_minute")
+                ));
     }
 
     public Activity selectByType(String type) {
         logger.debug("Selecting activity " + type);
-        return jdbcTemplate.query("SELECT * FROM activity WHERE name = ? LIMIT 1", this::rowMapper, type)
-                .stream()
-                .findFirst()
-                .orElse(null);
+        return jdbcTemplate.query(
+                "SELECT * FROM activity WHERE type = ? LIMIT 1",
+                this::rowMapper, type
+        ).stream().findFirst().orElse(null);
     }
 
     public Activity insert(NewActivity activity) {
         logger.debug("Inserting new activity: " + activity);
         return jdbcTemplate.query(
                 "SELECT * FROM FINAL TABLE (INSERT INTO activity (name) VALUES (?))",
-                this::rowMapper,
-                activity.name()
+                this::rowMapper, activity.name()
         ).stream().findFirst().orElse(null);
     }
 
@@ -71,9 +72,7 @@ public class ActivityJdbcRepository {
         logger.debug("Updating activity: " + activity);
         return jdbcTemplate.query(
                 "SELECT * FROM FINAL TABLE (UPDATE activity SET name = ? WHERE id = ?)",
-                this::rowMapper,
-                activity.name(),
-                activity.id()
+                this::rowMapper, activity.name(), activity.id()
         ).stream().findFirst().orElse(null);
     }
 
