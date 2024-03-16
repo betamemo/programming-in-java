07a9e18b1a10ffc5177378d8d80bbfca5f981db3
betamemo <bandita.kul@gmail.com>
2024-03-16 23:40:20 +0700

    add comment

--- m3-spring-application/src/main/java/com/harbourspace/tracker/activity/model/Activity.java
+++ m3-spring-application/src/main/java/com/harbourspace/tracker/activity/model/Activity.java
@@ -1,11 +1,11 @@
 package com.harbourspace.tracker.activity.model;
 
 public record Activity(
-        Long id,
-        Long userId,
-        String type,
-        String name,
-        double kcalPerMinute
+        Long id,                // Unique identifier of the activity.
+        Long userId,            // The unique identifier of the user who added the activity (0 for SYSTEM)
+        String type,            // The type of the activity (SYSTEM or USER).
+        String name,            // The name of the activity.
+        double kcalPerMinute    // The number of kilocalories burned per minute during the activity.
 ) {
     public Activity copyWithId(Long id) {
         return new Activity(id, userId, type, name, kcalPerMinute);
