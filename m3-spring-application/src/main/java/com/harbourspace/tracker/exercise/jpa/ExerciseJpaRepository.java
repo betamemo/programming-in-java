72328fcb21230192159cd9063c84c6306379d57b
betamemo <bandita.kul@gmail.com>
2024-03-22 16:08:30 +0700

    delete method

--- m3-spring-application/src/main/java/com/harbourspace/tracker/exercise/jpa/ExerciseJpaRepository.java
+++ m3-spring-application/src/main/java/com/harbourspace/tracker/exercise/jpa/ExerciseJpaRepository.java
@@ -1,17 +1,12 @@
 package com.harbourspace.tracker.exercise.jpa;
 
+import com.harbourspace.tracker.exercise.model.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ExerciseJpaRepository extends JpaRepository<ExerciseEntity, Long> {
 
-    List<ExerciseEntity> findById(long id);
-
     List<ExerciseEntity> findByUserId(long userId);
-
-    List<ExerciseEntity> findByActivityId(long activityId);
-
-    List<ExerciseEntity> findByDuration(long duration);
 
 }
